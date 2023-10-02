93b8918e5575af1a30ee43cad945ff6e4ae0db9d
zagaouch <i.zagaouch@gmail.com>
2023-10-03 00:54:26 +0100

    task9

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+/**
+* main - creat zoombis
+*/
+int main(void)
+{
+	pid_t parent_pid = getpid();
+	pid_t child_pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid < 0)
+		{
+			perror("fork");exit(EXIT_FAILURE);
+		}
+		if (child_pid == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(EXIT_SUCCESS);
+		}
+	}
+	sleep(10);
+	return 0;
+}
+
