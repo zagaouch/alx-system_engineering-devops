999a17d3c59597521fdc5e15253af520bb59e8a7
zagaouch <i.zagaouch@gmail.com>
2024-02-20 21:38:27 +0100

    auto commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
-'''
-gather employee data from API
-'''
-
-import re
+""" get request """
+import json
 import requests
 import sys
 
-REST_API = "https://jsonplaceholder.typicode.com"
+def retrieve_todo_list_progress(emp_id):
+    """ returns todolist progress given employee id"""
+    todo_url = "https://jsonplaceholder.typicode.com"
+    emp_url = f"{todo_url}/todos?userId={emp_id}"
+    user_url = f"{todo_url}/users/{emp_id}"
+    try:
+        user_info = requests.get(user_url).json()
+        user_name = user_info.get('name')
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
-            task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp_name = req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    emp_name,
-                    len(completed_tasks),
-                    len(tasks)
-                )
-            )
-            if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print('\t {}'.format(task.get('title')))
+        todo_list = requests.get(emp_url).json()
+        todo_size = len(todo_list)
+
+        tasks_completed = []
+        for todo in todo_list:
+            if todo.get('completed') is True:
+                tasks_completed.append(todo.get('title'))
+
+        print("Employee {} is done with tasks({}/{}".format(
+            user_name, len(tasks_completed), len(todo_list)))
+        for todo in tasks_completed:
+            print("\t {}".format(todo))
+    except requests.exceptions.RequestException as int_rrror:
+        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(1)
+    retrieve_todo_list_progress(int(sys.argv[1]))
