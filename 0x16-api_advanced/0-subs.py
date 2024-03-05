3023ca70c2fee05119102bdb6de48dd1f2a855db
zagaouch <i.zagaouch@gmail.com>
2024-03-05 22:35:11 +0100

    auto commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""Query reddit api subscribers for a subredit"""
-
+'''
+    this module contains the function number_of_subscribers
+'''
 import requests
-from os import sys
-
-
-subredit = sys.argv[1]
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """fucntion return the number of subscribers for a given subreddit"""
-    if subreddit is None:
+    '''
+        returns the number of subscribers for a given subreddit
+    '''
+    user = {'User-Agent': 'Mozilla/5.0'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
+    try:
+        return url.get('data').get('subscribers')
+    except Exception:
         return 0
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        return 0
-    return response.json().get('data').get('subscribers')
 
 
 if __name__ == "__main__":
