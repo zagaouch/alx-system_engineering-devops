c6fbff2a5696928916df26250e8fb5f903c2bef9
zagaouch <i.zagaouch@gmail.com>
2024-03-05 22:02:00 +0100

    task0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+ function that queries the Reddit API and returns the number of subscribers
+"""
+
+import os
+import requests 
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
