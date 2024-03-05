c161ff5209c3949c57c47fadd803b035f861beee
zagaouch <i.zagaouch@gmail.com>
2024-03-05 22:03:11 +0100

    auto commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,6 +6,9 @@
 import os
 import requests 
 def number_of_subscribers(subreddit):
+    """
+     function that queries the Reddit API and returns the number of subscribers
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     
