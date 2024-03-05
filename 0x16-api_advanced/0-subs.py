00e1d14d334a4ac340443c395ee9befd23b61d78
zagaouch <i.zagaouch@gmail.com>
2024-03-05 22:33:16 +0100

    auto commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,14 @@ from os import sys
 
 
 subredit = sys.argv[1]
+
+
 def number_of_subscribers(subreddit):
     """fucntion return the number of subscribers for a given subreddit"""
     if subreddit is None:
         return 0
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'hello'}
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
