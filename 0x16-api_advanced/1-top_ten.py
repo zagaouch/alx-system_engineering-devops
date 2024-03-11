1f486a4f93e17c7a906b536104822fb010b24d91
zagaouch <i.zagaouch@gmail.com>
2024-03-11 15:27:19 +0000

    task1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-"""
-    Query reddit api top ten- posts titles for a subredit
-"""
-
+'''
+    this module contains the function top_ten
+'''
 import requests
-from os import sys
-
-subredit = sys.argv[1]
+from sys import argv
 
 
 def top_ten(subreddit):
-    """
-        Get top 10 hot posts titles for a given subreddit
-    """
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    '''
+        function returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Mozilla/5.0'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            return 0
-        posts = response.json().get('data').get('children')
-        for post in posts:
-            print(post.get('data').get("title"))
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
     except Exception:
-        print("None")
+        print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
