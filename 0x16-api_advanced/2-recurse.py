70ebd6c396d44afa03f9d40110f3efca8a46abf4
zagaouch <i.zagaouch@gmail.com>
2024-03-11 15:51:01 +0000

    auto commit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,9 +4,9 @@ Recursive function that queries
 the Reddit API and returns
 a list containing the
 titles of all hot articles for a given subreddit.
-If no results are 
+If no results are
 found for the given subreddit,
-the function 
+the function
 should return None.
 """
 
